9b43bb077caf9303ce3756e210a289fd3b484a26
Nancy Salaiau <nsalaiau@byupathway.edu>
2026-09-23 09:18:31 +1000

    Add Comments class with properties and constructor

--- week04/Video.cs/Comments.cs
+++ week04/Video.cs/Comments.cs
@@ -1,1 +1,11 @@
-
+public class Comments
+{
+    public string Text { get; set; } 
+    public string Author { get; set; } 
+    
+    public Comments(string text, string author)
+    {
+        Text = text;
+        Author = author;
+    }
+}
